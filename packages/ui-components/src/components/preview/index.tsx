c64df47dc6181af83941acdd4eeb525c43602988
qqqzhch <qqqzhch@163.com>
2023-06-28 10:59:43 +0800

    feat: status text

--- packages/ui-components/src/components/preview/index.tsx
+++ packages/ui-components/src/components/preview/index.tsx
@@ -146,7 +146,7 @@ const PreviewModal: FC<componentprops> = ({ isOpen, closeModal }) => {
                       className="inline-flex flex-1 justify-center rounded-md border border-transparent bg-blue-500 px-4 py-2 text-sm font-medium  text-white hover:bg-blue-600 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
                      
                     >
-                     Verfying...({statusMint.text})
+                     Verfying...{statusMint.text}
                     </button>
 
                       </Then>
