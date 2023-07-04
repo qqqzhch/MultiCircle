699d100865d54c4bd01604b25a72ad04663eb764
qqqzhch <qqqzhch@163.com>
2023-07-04 15:23:20 +0800

    style: input style

--- packages/ui-components/src/components/swap/index.tsx
+++ packages/ui-components/src/components/swap/index.tsx
@@ -188,18 +188,21 @@ const Swap = () => {
          
         </div>
         </div>
-        <div className="relative z-0 w-full mb-6 group  flex flex-row justify-between">
+        <div className="z-0 w-full mb-6  ">
+          <div className=' flex flex-row justify-between space-x-2'>
+
+          
           <div className=' flex-1 '>
           <input
             type="text"
             name="input"
-            className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-            placeholder="0"
+            className="bg-gray-50 border  outline-none  border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="0.0"
             required
          
             onChange={(e)=>{inputAmountChange(e.currentTarget.value)}}
           />
-          <p className=' text-red-400'>{inputError}</p>
+         
           </div>
           <div className=' flex   flex-row  text-gray-500 items-center justify-end'>
             <div className=' flex flex-col mr-2 text-sm   text-gray-400'>
@@ -217,24 +220,26 @@ const Swap = () => {
             
             <img className=' w-10 h-10' src={usdclogo}></img>
           </div>
-          
-          <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+
+          </div>
+          {/* <label className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
             input
-          </label>
+          </label> */}
+           <p className='mt-2 text-sm text-red-600 dark:text-red-500'>{inputError}</p>
         </div>
-        
-        
-        
        
-        <div className="relative z-0 w-full  group  mb-10">
+        
+        
+
+        <div className="flex items-center mb-2">
           
-          <label className="peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
+          <label className="ml-2 text-sm font-medium text-gray-500 dark:text-gray-300">
           You will receive: {" "}  {inputAmount} USDC
           </label>
         </div>
-        <div className="relative z-0 w-full  group mb-1 flex flex-row text-sm text-gray-500">
-        <div>Protocol Fee:</div>  
-          <div className='  min-w-[50px]'>
+        <div className="flex items-center mb-2">
+          <div className='ml-2 text-sm font-medium text-gray-500 dark:text-gray-300'>Protocol Fee:</div>  
+          <div className=' min-w-[50px] ml-2 text-sm font-medium text-gray-500 dark:text-gray-300'>
              
           <If condition={RelayerFee.isloading}>
             <Then>
@@ -246,14 +251,11 @@ const Swap = () => {
             </Else>
           </If>
           </div>   
-          <div className=" flex flex-row peer-focus:font-medium absolute text-lg text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">
-        
-       
-          </div>
+   
         </div>
-        <div className="relative z-0 w-full  group mb-14 flex flex-row text-sm text-gray-500">
-        <div>Gas Fee:</div>  
-          <div className='  min-w-[50px]'>
+        <div className="flex items-center mb-2">
+        <div className='ml-2 text-sm font-medium text-gray-500 dark:text-gray-300'>Gas Fee:</div>  
+          <div className='  min-w-[50px] ml-2 text-sm font-medium text-gray-500 dark:text-gray-300'>
              
           <If condition={gasFeeLoading}>
             <Then>
