a28486f276b55be26f6172024f90913381989310
qqqzhch <qqqzhch@163.com>
2023-08-10 14:23:22 +0800

    feat: route

--- packages/web/src/route.tsx
+++ packages/web/src/route.tsx
@@ -2,14 +2,14 @@ import { createBrowserRouter, RouterProvider, Route, createRoutesFromElements} f
 
 import App from './App'
 import ErrorPage from './views/errorpage'
-import Home from './views/Home'
+// import Home from './views/Home'
 import NoMatch from './views/noMatch'
 import Swap from './views/Swap'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
      <Route path="/" element={<App />} errorElement={<ErrorPage />}   >
-        <Route path="swap" element={<Home />} />
+        {/* <Route path="swap" element={<Home />} /> */}
         <Route path="" element={<Swap />} />
         
         {/* <Route path="login" element={<Login />} />
