bbb611183bf77ff5615fbff3a4d0804fe25a1961
qqqzhch <qqqzhch@163.com>
2023-08-14 08:44:26 +0800

    feat: 🎸 cache data

--- packages/ui-components/src/hooks/useTxStatus.ts
+++ packages/ui-components/src/hooks/useTxStatus.ts
@@ -5,13 +5,25 @@ import { BaseUrl } from '../constants/relayer'
 import api from '../api/fetch'
 import { SearchTxhash } from '../types/searchTxhash'
 
+type cacheType = {
+  [key: string]: SearchTxhash
+}
+const cacheData: cacheType = {}
+
 async function fetcher(txhash: string | undefined | null): Promise<SearchTxhash | undefined> {
   if (txhash == null || txhash == undefined) {
     return
   }
+  if (cacheData[txhash]) {
+    return cacheData[txhash]
+  }
 
   const res = await api.get<SearchTxhash>(BaseUrl + '/search/?txhash=' + txhash)
+
   if (res && res.code == 0 && res.data) {
+    if (res.data.status == 'success') {
+      cacheData[txhash] = res
+    }
     return res
   } else {
     throw new Error('get Accounts info error ')
